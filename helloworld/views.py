739bd8a747436c6d4b8c2a171c81833a4cc3ee98
Drew Dru <drew.drux@gmail.com>
2020-11-03 10:52:28 +0300

    add hello world microservice

---
+++ helloworld/views.py
@@ -0,0 +1,66 @@
+from typing import Optional
+from fastapi import APIRouter, Query, Body, Path
+
+from helloworld.models import Item
+
+helloworld = APIRouter()
+
+@helloworld.get("/",
+    tags=["main"],
+    responses={
+        200: {
+            "description": "Return Hello World.",
+        }
+    },
+)
+async def read_root() -> dict:
+    """Hello world"""
+    return {"Hello": "World"}
+
+
+@helloworld.get("/items/{item_id}",
+    tags=["main"],
+    responses={
+        200: {
+            "description": "Item Data.",
+        }
+    },
+)
+async def read_item(
+        item_id: int = Path(
+            None,
+            title="Item ID",
+            description="Item Identifier"
+        ),
+        q: Optional[str] = Query(
+            None,
+            title="Query string",
+            description="Query string for the items to search in the database that have a good match",
+        )
+    ) -> dict:
+    """Get Item"""
+    return {"item_id": item_id, "q": q}
+
+
+@helloworld.put("/items/{item_id}", tags=["main"],
+    status_code=201,
+    responses={
+        201: {
+            "description": "Item Data.",
+        }
+    },
+)
+async def update_item(
+        item_id: int = Path(
+            None,
+            title="Item ID",
+            description="Item Identifier"
+        ),
+        item: Item = Body(
+            None,
+            title="Item",
+            description="New Item Data",
+        )
+    ) -> dict:
+    """Update Item"""
+    return {"item_name": item.name, "item_id": item_id}
