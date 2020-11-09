75741245a1933f596e2a71ac776bcbc8000b8ac2
Drew Dru <drew.drux@gmail.com>
2020-11-10 01:31:03 +0300

    add middlewares

--- main.py
+++ main.py
@@ -1,41 +1,36 @@
 import logging
 
-import sentry_sdk
 import uvicorn
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from fastDrewDru.config import get_settings
 from fastDrewDru.db import db
 from helloworld.views import helloworld
+from middlewares import SentryMiddleware
 from movies.views import movies
 
 logging.config.fileConfig("logging.conf", disable_existing_loggers=False)
 logger = logging.getLogger(__name__)
 settings = get_settings()
 
-sentry_sdk.init(settings.SENTRY_DNS, traces_sample_rate=1.0)
-
 app = FastAPI(
     title="drewdru.com",
     description="REST API for drewdru.com",
     version="0.1.0",
 )
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app.add_middleware(SentryMiddleware, dns=settings.SENTRY_DNS, traces_sample_rate=1.0)
+
 app.include_router(movies, prefix="/movies", tags=["movies"])
 app.include_router(helloworld, prefix="/helloworld", tags=["helloworld"])
-
-
-@app.middleware("http")
-async def sentry_exception(request: Request, call_next):
-    try:
-        response = await call_next(request)
-        return response
-    except Exception as e:
-        with sentry_sdk.push_scope() as scope:
-            scope.set_context("request", request)
-            user_id = "database_user_id"  # when available
-            scope.user = {"ip_address": request.client.host, "id": user_id}
-            sentry_sdk.capture_exception(e)
-        raise e
 
 
 @app.on_event("startup")
@@ -48,7 +43,7 @@ async def shutdown():
     await db.disconnect()
 
 
-@app.put("/", tags=["home"])
+@app.get("/", tags=["home"])
 async def home():
     logger.info("logging from the root logger")
     return {"version": "0.0.1"}
---
+++ middlewares.py
@@ -0,0 +1,35 @@
+import logging
+
+import sentry_sdk
+from fastapi import Request
+from sentry_sdk.integrations.logging import LoggingIntegration
+from starlette.middleware.base import BaseHTTPMiddleware
+
+logging.config.fileConfig("logging.conf", disable_existing_loggers=False)
+logger = logging.getLogger(__name__)
+
+
+class SentryMiddleware(BaseHTTPMiddleware):
+    def __init__(self, app=None, dns="", *args, **kwargs):
+        super().__init__(app)
+        sentry_logging = LoggingIntegration(
+            level=logging.INFO,  # Capture info and above as breadcrumbs
+            event_level=logging.ERROR,  # Send errors as events
+        )
+        kwargs["integrations"] = [sentry_logging]
+        sentry_sdk.init(dns, kwargs)
+
+    async def dispatch(self, request: Request, call_next):
+        try:
+            return await call_next(request)
+        except Exception as e:
+            with sentry_sdk.push_scope() as scope:
+                scope.set_context("request", request)
+                user_id = "test_user_id"  # when available
+                scope.user = {
+                    "ip_address": request.client.host,
+                    "id": user_id,
+                    "name": "test_user",
+                }
+                sentry_sdk.capture_exception(e)
+            raise e
