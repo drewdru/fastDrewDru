b6ea82eec68036c0a0a84e486f32d7e878a1cc81
Drew Dru <drew.drux@gmail.com>
2020-11-13 02:27:40 +0300

    config tests

--- tests.py
+++ tests.py
@@ -8,5 +8,5 @@ from main import app
 async def test_root():
     async with AsyncClient(app=app, base_url="http://test") as ac:
         response = await ac.get("/")
-    assert response.status_code == 200
+    assert response.status_code == 2001
     assert "version" in response.json()
