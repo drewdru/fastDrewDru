f6eafd8ab560dada67d22cee880cd7203d332c76
Drew Dru <drew.drux@gmail.com>
2020-11-13 01:33:41 +0300

    get api version

--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Response, status
 
 from movies import crud
 from movies.schemas import MovieIn, MovieOut
@@ -8,34 +8,31 @@ from movies.schemas import MovieIn, MovieOut
 movies = APIRouter()
 
 
-@movies.get("/", response_model=List[MovieOut])
-async def index():
+@movies.get("/", status_code=status.HTTP_200_OK, response_model=List[MovieOut])
+async def index() -> Response:
     return await crud.get_all_movies()
 
 
-@movies.post("/", status_code=201)
-async def add_movie(payload: MovieIn):
+@movies.post("/", status_code=status.HTTP_201_CREATED, response_model=MovieOut)
+async def add_movie(payload: MovieIn) -> Response:
     movie_id = await crud.add_movie(payload)
     return {"id": movie_id, **payload.dict()}
 
 
-@movies.put("/{id}")
-async def update_movie(id: int, payload: MovieIn):
+@movies.put("/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def update_movie(id: int, payload: MovieIn) -> Response:
     movie = await crud.get_movie(id)
     if not movie:
-        raise HTTPException(status_code=404, detail="Movie not found")
-
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Movie not found")
     update_data = payload.dict(exclude_unset=True)
     movie_in_db = MovieIn(**movie)
-
     updated_movie = movie_in_db.copy(update=update_data)
-
     return await crud.update_movie(id, updated_movie)
 
 
-@movies.delete("/{id}")
-async def delete_movie(id: int):
+@movies.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_movie(id: int) -> Response:
     movie = await crud.get_movie(id)
     if not movie:
-        raise HTTPException(status_code=404, detail="Movie not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Movie not found")
     return await crud.delete_movie(id)
