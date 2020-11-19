258dfe917f45e40399d577249db81d4ea9db5933
Drew Dru <drew.drux@gmail.com>
2020-11-19 23:59:55 +0300

    add docker, fix docs, configure project

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+case $1 in
+  dev)
+    cd /home/drewdru/develop/python/backendDrewdru/
+    source env/bin/activate
+    python manage.py migrations upgrade head
+    python manage.py run
+    ;;
+  prod)
+    cd /home/drewdru/develop/python/backendDrewdru/
+    source env/bin/activate
+    python manage.py migrations upgrade head --prod
+    python manage.py run --prod
+    ;;
+  docker)
+    python manage.py migrations upgrade head
+    python manage.py run
+    ;;
+esac
